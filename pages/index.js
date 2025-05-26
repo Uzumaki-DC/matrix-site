6fc9518325c2656ee9f4a2a5c696745307dde0d9
Uzumaki-DC <uzumaki.dc@gmail.com>
2025-05-26 08:22:55 +0200

    chore: manual Next.js setup with basic page and global styles

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Welcome to the Matrix Site</h1>
+      <p>Your Next.js app is up and running.</p>
+    </div>
+  );
+}
